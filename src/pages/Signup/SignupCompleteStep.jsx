2ab6616421256c821dc80c8445acbdb0db39765c
LeeJS9856 <wnstj1057@naver.com>
2025-01-12 18:49:23 +0900

    Refact : Signup Complete 페이지 container 전역스타일로 변경

--- src/pages/Signup/SignupCompleteStep.jsx
+++ src/pages/Signup/SignupCompleteStep.jsx
@@ -4,7 +4,7 @@ import style from './Signup.module.css';
 
 const SignupCompleteStep = () => {
     return (
-        <div className={style.completeContainer}>
+        <div className="centerContainer">
             <ion-icon name="checkmark-circle-outline"></ion-icon>
             <p className={style.infoText}>회원가입 완료</p>
             <p className={style.infoTextGray}>회원가입이 성공적으로 완료되었어요</p>
