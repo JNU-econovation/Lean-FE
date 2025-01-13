93743caa58d9848a24c7af8c94451794fb38569c
LeeJS9856 <wnstj1057@naver.com>
2025-01-14 03:22:24 +0900

    Refact : Complete 페이지 컴포넌트화

--- src/pages/RentalItem/RentalItemComplete.jsx
+++ src/pages/RentalItem/RentalItemComplete.jsx
@@ -1,14 +1,8 @@
-import style from './RentalItemComplete.module.css';
-import Button from '../../components/Button/Button';
+import CompletePage from '../../components/page/CompletePage';
 
 const RentalItemComplete = () => {
     return (
-        <div className="centerContainer">
-            <ion-icon name="checkmark-circle-outline"></ion-icon>
-            <p className={style.infoText}>예약 완료</p>
-            <p className={style.infoTextGray}>예약이 성공적으로 완료되었어요</p>
-            <Button text="메인으로" type="button" className={`fixWidth ${style.mainButton}`} link='/main'/>
-        </div>
+        <CompletePage title='예약 완료' description='예약이 성공적으로 완료되었어요' buttonText='메인으로' link='/main'/>           
     )
 };
 
--- src/pages/Signup/SignupCompleteStep.jsx
+++ src/pages/Signup/SignupCompleteStep.jsx
@@ -1,20 +1,9 @@
-import Button from '../../components/Button/Button';
-import PropTypes from 'prop-types';
-import style from './Signup.module.css';
+import CompletePage from "../../components/page/CompletePage";
 
 const SignupCompleteStep = () => {
     return (
-        <div className="centerContainer">
-            <ion-icon name="checkmark-circle-outline"></ion-icon>
-            <p className={style.infoText}>회원가입 완료</p>
-            <p className={style.infoTextGray}>회원가입이 성공적으로 완료되었어요</p>
-            <Button text="로그인하러 가기" type="button" className={`fixWidth ${style.signupButton}`} link='/signin'/>
-        </div>
+        <CompletePage title='회원가입 완료' description='회원가입이 성공적으로 완료되었어요' buttonText='로그인하러 가기' link='/signin'/>
     );
-};
-
-SignupCompleteStep.propTypes = {
-    navigate: PropTypes.func
 };
 
 export default SignupCompleteStep;
