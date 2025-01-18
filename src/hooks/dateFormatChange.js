29e1dce7dac0e97986a9b743e741f6386bf557c4
LeeJS9856 <wnstj1057@naver.com>
2025-01-18 16:43:28 +0900

    Feat : dateformat 변경 훅 추가

--- src/hooks/dateFormatChange.js
+++ src/hooks/dateFormatChange.js
@@ -1,13 +1,35 @@
-export const formatTimeDifference = (timestamp) => {
+export const formatDDay = (timestamp) => {
     if (!timestamp) {
         return '유효하지 않은 날짜';
     }
     const now = new Date();
     const targetTime = new Date(timestamp);
 
-    if (targetTime > now) {
-        return '만료되었습니다';
-    }
+    const difference = targetTime - now;
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    let result = '';
+    if (days > 0) result += `${days}D`;
+    if (hours > 0 || days === 0) result += ` ${hours}H`;
+
+    return result;
+}
+
+export const formatDate = (timestamp) => {
+    const date = new Date(timestamp);
+
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); 
+    const day = date.getDate().toString().padStart(2, '0');
+
+    return `${year}/${month}/${day}`;
+}
+
+export const formatOverDate = (timestamp) => {
+    const now = new Date();
+    const targetTime = new Date(timestamp);
 
     const difference = now - targetTime;
 
