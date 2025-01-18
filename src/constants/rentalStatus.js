a57014eca529013d9eb5f330d871352f3fb28505
LeeJS9856 <wnstj1057@naver.com>
2025-01-19 02:21:31 +0900

    Design : 처리중 라벨 색깔 변경

--- src/constants/rentalStatus.js
+++ src/constants/rentalStatus.js
@@ -1,7 +1,7 @@
 const RENTAL_STATUS = {
     대기중: { label: '대기중', color: '#7dc5b4' },
     대여중: { label: '대여중', color: '#82b5f5' },
-    처리중: { label: '처리중', color: '#a488e5' },
+    처리중: { label: '처리중', color: '#82b5f5' },
     반납: { label: '반납', color: '#a488e5' },
     만료: { label: '만료', color: '#ec8886' },
 };
