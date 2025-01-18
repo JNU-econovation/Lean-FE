b114e0fb48e2e11b38a1e5aa2a69b5ada3632f28
LeeJS9856 <wnstj1057@naver.com>
2025-01-18 15:23:52 +0900

    Rename : RentalStatus값 이름 수정

--- src/constants/rentalStatus.js
+++ src/constants/rentalStatus.js
@@ -1,8 +1,8 @@
 const RENTAL_STATUS = {
-    RESERVED: { label: '예약', color: '#7dc5b4' },
-    RENTED: { label: '대여', color: '#82b5f5' },
-    EXPIRED: { label: '만료', color: '#ec8886' },
-    RETURNED: { label: '반납', color: '#a488e5' },
+    대기중: { label: '대기중', color: '#7dc5b4' },
+    대여중: { label: '대여중', color: '#82b5f5' },
+    만료: { label: '만료', color: '#ec8886' },
+    반납: { label: '반납', color: '#a488e5' },
 };
 
 export default RENTAL_STATUS;
