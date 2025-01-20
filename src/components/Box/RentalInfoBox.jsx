d59dcfd27637bdf1400f3eb39da566ed127f4931
LeeJS9856 <wnstj1057@naver.com>
2025-01-20 18:07:55 +0900

    Feat : RentalInfoBox 관리자 승인 버튼 처리

--- src/components/Box/RentalInfoBox.jsx
+++ src/components/Box/RentalInfoBox.jsx
@@ -1,8 +1,26 @@
 import style from './RentalInfoBox.module.css';
 import PropTypes from 'prop-types';
 import { formatDate, formatDDay, formatOverDate } from '../../hooks/dateFormatChange';
+import ConfirmDialog from '../Dialog/ConfirmDialog';
+import { useState } from 'react';
+import apiClient from '../../services/apiClient';
+import { showError } from '../../hooks/useSignupValidation';
 
-const RentalInfoBox = ({ rentalInfo, isStudentCouncil = false }) => {
+const RentalInfoBox = ({ rentalInfo, isStudentCouncil = false, rentalId }) => {
+    const [rentalDialogOpen, setRentalDialogOpen] = useState(false);
+    const [returnDialogOpen, setReturnDialogOpen] = useState(false);
+    const handleRental = async () => {
+        try {
+            await apiClient.put(`/api/v1/rentals/details/${rentalId}`);
+            setRentalDialogOpen(false);
+            setReturnDialogOpen(false);
+            window.location.reload();
+        } catch (error) {
+            console.error('상태 변경 실패:', error);
+            showError(null, '상태 변경에 실패했습니다. 다시 시도해주세요.');
+        }
+    }
+
     const renderInfo = (label, value, extraStyle = '') => (
         <div>
             <p className={style.info}>{label}</p>
@@ -21,10 +39,28 @@ const RentalInfoBox = ({ rentalInfo, isStudentCouncil = false }) => {
                 {renderInfo('예약 만료 일자', formatDate(reservationExpirationDate))}
                 {renderInfo('대여 장소', studentCouncilAddress)}
                 {renderInfo('대여 물품', itemName)}
+                {
+                    isStudentCouncil?
+                    <div className={style.infoBox}>
+                        <p className={`${style.info} ${style.reservation}`}>신청 승인</p>
+                        <ion-icon name="chevron-forward" onClick={() => setRentalDialogOpen(true)}></ion-icon>
+                    </div>: ''
+                }
+                
                 </>
             );
     
             case '대여중':
+                return (
+                    <>
+                    {renderInfo('대여 일자', formatDate(rentalStartDate))}
+                    {renderInfo('만료 일자', formatDate(rentalExpireDate))}
+                    {renderInfo('반납 장소', studentCouncilAddress)}
+                    {renderInfo('대여 물품', itemName)}
+                    {renderInfo('반납 기한', formatDDay(rentalExpireDate), style.rentalDate)}
+                    
+                    </>
+                );
             case '처리중':
             return (
                 <>
@@ -33,6 +69,13 @@ const RentalInfoBox = ({ rentalInfo, isStudentCouncil = false }) => {
                 {renderInfo('반납 장소', studentCouncilAddress)}
                 {renderInfo('대여 물품', itemName)}
                 {renderInfo('반납 기한', formatDDay(rentalExpireDate), style.rentalDate)}
+                {
+                    isStudentCouncil?
+                    <div className={style.infoBox}>
+                        <p className={`${style.info} ${style.rentalDate}`}>반납 승인</p>
+                        <ion-icon name="chevron-forward" onClick={() => setReturnDialogOpen(true)}></ion-icon>
+                    </div>: ''
+                }
                 </>
             );
     
@@ -54,6 +97,13 @@ const RentalInfoBox = ({ rentalInfo, isStudentCouncil = false }) => {
                 {renderInfo('반납 장소', studentCouncilAddress)}
                 {renderInfo('대여 물품', itemName)}
                 {renderInfo('초과 일자', `${formatOverDate(rentalExpireDate)} 초과`, style.expirationDate)}
+                {
+                    isStudentCouncil?
+                    <div className={style.infoBox}>
+                        <p className={`${style.info} ${style.expirationDate}`}>반납 승인</p>
+                        <ion-icon name="chevron-forward" onClick={() => setReturnDialogOpen(true)}></ion-icon>
+                    </div>: ''
+                }
                 </>
             );
     
@@ -63,10 +113,38 @@ const RentalInfoBox = ({ rentalInfo, isStudentCouncil = false }) => {
     };
   
     return (
-      <div className={style.container}>
-        {renderRentalInfo()}
-        {isStudentCouncil && renderInfo('추후 처리', 'N/A')}
-      </div>
+        <div className={style.container}>
+            {renderRentalInfo()}
+            <ConfirmDialog
+                    title="대여 승인"
+                    description={
+                        <>
+                            대여 일시는 {formatDate(rentalInfo.reservationStartDate)} <br />
+                            대여 물품은 {rentalInfo.itemName} 입니다. <br />
+                            대여를 승인하시겠습니까?
+                        </>
+                    }
+                    closeText="거절"
+                    confirmText="승인"
+                    open={rentalDialogOpen}
+                    onClose={() => setRentalDialogOpen(false)}
+                    onConfirm={handleRental}
+                    isRental={true}/>
+            <ConfirmDialog
+                    title="반납 승인"
+                    description={
+                        <>
+                            반납 물품은 {rentalInfo.itemName} 입니다. <br />
+                            반납을 승인하시겠습니까?
+                        </>
+                    }
+                    closeText="거절"
+                    confirmText="승인"
+                    open={returnDialogOpen}
+                    onClose={() => setReturnDialogOpen(false)}
+                    onConfirm={handleRental}
+                    isRental={true}/>
+        </div>
     );
   };
 
@@ -82,6 +160,7 @@ RentalInfoBox.propTypes = {
         itemName: PropTypes.string,
     }).isRequired,
     isStudentCouncil: PropTypes.bool,
+    rentalId: PropTypes.string,
 };
 
 export default RentalInfoBox;
